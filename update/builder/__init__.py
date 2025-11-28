0528a06752d914c862db1aaa81ee3dbf93cf5c08
Izzat Alharis <izzatalharist@gmail.com>
2025-11-28 22:08:59 +0700

    Add update capability

--- update/builder/__init__.py
+++ update/builder/__init__.py
@@ -1,30 +1,37 @@
-import subprocess
-import platform
+from os import walk, path, makedirs
+from datetime import datetime
+import shutil
+
+update_from_dir: str = '../payload'
+update_to_dir: str = '../../dist'
 
 
-# Kill nodejs process
-def kill_server():
-    os = platform.system()
-
-    command = []
-
-    if os == 'Windows':
-        command += ["taskkill", "/F", "/IM", "node.exe"]
-
-    elif os == "Linux" or os == "Darwin":
-        command += ["pkill", "node"]
-
-    try:
-        subprocess.run(command)
-    except:
-        print('No')
-        pass
+def log(txt: str):
+    date = datetime.now()
+    print(date)
+    print(txt)
+    print('-'*50)
 
 
 def update_payload():
-    pass
+    if not path.exists(update_from_dir):
+        return log('Folder "dist" tidak ditemukan!')
+
+    for root, subfolders, files in walk(update_from_dir):
+        for f in files:
+            file_src = path.join(root, f)
+            file_dest = path.join(root.replace(update_from_dir, update_to_dir), f)
+            file_dir = path.dirname(file_dest)
+
+            # Create folder if not exits
+            if not path.exists(file_dir):
+                log(f'Creating folder "{file_dir}"')
+                makedirs(file_dir)
+
+            # Copy file
+            log(f'Copy file...\nFrom "{file_src}"\nTo "{file_dest}"')
+            shutil.copy(file_src, file_dest)
 
 
 if __name__ == "__main__":
-    kill_server()
     update_payload()
