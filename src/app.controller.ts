8c1b2bea87f86db19b7268df33c6f1ff5f5446ed
Izzat Alharis <izzatalharist@gmail.com>
2025-11-30 09:34:05 +0700

    Add shell scripting support & fix updater

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,116 @@
 import {
     InternalServerErrorException,
     UnauthorizedException,
+    BadRequestException,
     UseInterceptors,
-    UploadedFiles,
+    UploadedFile,
     Controller,
     Param,
     Post,
+    Body,
     Get,
+    Query,
 } from "@nestjs/common";
-import { FilesInterceptor } from "@nestjs/platform-express";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { executeUpdate } from "./libs/updater";
+import { existsSync, mkdirSync, readdirSync } from "fs";
+import { IsNotEmpty } from "class-validator";
 import { AppService } from "./app.service";
 import {
     GetFileDestBeforeUpload,
     upload_update_from_dir,
     UploadFile,
 } from "./libs/upload-file-handler";
-import { extname } from "path";
+import { extname, join } from "path";
+import { glob } from "glob";
+import { ParseUrlQuery } from "./libs/string";
 
-@Controller()
-export class AppController {
+class UploadPayloadDto {
+    @IsNotEmpty()
+    file_path: string;
+}
+
+// Nanti harus ditambahkan fitur autentikasi
+class ShellCommands {
+    private readonly wrong_target_err: string = "Wrong target path!";
+    private readonly create_dir_err: any = {
+        exists: "Folder is already exist!",
+        not_specified: "We expected folder name!",
+    };
+
     constructor(private readonly service: AppService) {}
 
+    @Get("sh-ls-dir/:target")
+    shLsDir(@Param("target") target): string[] {
+        if (!target || !existsSync(target)) {
+            throw new BadRequestException(this.wrong_target_err);
+        }
+        return readdirSync(target);
+    }
+
+    @Get("sh-extract-dir/:target")
+    async shExtractDir(@Param("target") target): Promise<string[]> {
+        if (!target || !existsSync(target)) {
+            throw new BadRequestException(this.wrong_target_err);
+        }
+        return glob(target + "/**/*");
+    }
+
+    @Get("sh-create-dir")
+    shCreateDir(@Query() args: any): string {
+        /* ===========================
+        | Example:
+        | ===========================
+
+        | ---------------------------
+        | Single directory
+        | ---------------------------
+        | 1. dir-name
+        | ?single=dir-name
+        | 2. dir-name/sub-dir-name
+        | ?single=dir-name/sub-dir-name
+        |
+        | ---------------------------
+        | Multi directory
+        | ---------------------------
+        | 1. dir-name
+        | 1. dir-name/sub-dir-name/anything
+        | => ?multi=['dir-name', '/dir-name/sub-dir-name/anything']
+        */
+        const query = ParseUrlQuery(args);
+
+        // No folder specified
+        if (!query.single && !query.multi) {
+            throw new BadRequestException(this.create_dir_err.not_specified);
+        }
+
+        let q: any = [];
+
+        // Single dir
+        if (query.single) {
+            q.push(query.single);
+        }
+
+        // Multi dir
+        if (query.multi) {
+            q = [...q, ...query.multi];
+        }
+
+        for (let qx of q) {
+            try {
+                mkdirSync(qx, { recursive: true });
+            } catch (error) {
+                throw new InternalServerErrorException(error);
+            }
+        }
+
+        return q;
+    }
+}
+
+@Controller()
+export class AppController extends ShellCommands {
+    // Nanti harus ditambahkan fitur autentikasi (hanya admin yang boleh meng-update server)
     @Get("update-execute/:dev_code")
     updateExecute(@Param("dev_code") dev_code: string) {
         // Wrong developer key not presented
@@ -34,12 +123,14 @@ export class AppController {
         return executeUpdate();
     }
 
+    // Nanti harus ditambahkan fitur autentikasi (hanya admin yang boleh meng-update server)
     @Post("update-payload/:dev_code")
-    @UseInterceptors(FilesInterceptor("files"))
+    @UseInterceptors(FileInterceptor("file"))
     updatePayload(
         @Param("dev_code") dev_code: string,
-        @UploadedFiles()
-        files: Array<Express.Multer.File>,
+        @UploadedFile()
+        file: Express.Multer.File,
+        @Body() { file_path }: UploadPayloadDto,
     ) {
         // Wrong developer key not presented
         if (!dev_code || dev_code != process.env.APP_UPDATE_PAYLOAD_DEVCODE) {
@@ -47,20 +138,23 @@ export class AppController {
             throw new UnauthorizedException();
         }
 
-        for (let f of files) {
-            const fext: string = extname(f.originalname);
-            const fname: string = f.originalname.replace(fext, "");
-            const file_dest: string = GetFileDestBeforeUpload(
-                f,
-                upload_update_from_dir,
-                fname,
-            );
+        // No file uploaded
+        if (!file) {
+            throw new BadRequestException("Wajib mengunggah file!");
+        }
 
-            try {
-                UploadFile(f, file_dest);
-            } catch (error) {
-                throw new InternalServerErrorException(error);
-            }
+        const fext: string = extname(file.originalname);
+        const fname: string = file.originalname.replace(fext, "");
+        const file_dest: string = GetFileDestBeforeUpload(
+            file,
+            join(upload_update_from_dir, file_path),
+            fname,
+        );
+
+        try {
+            UploadFile(file, file_dest);
+        } catch (error) {
+            throw new InternalServerErrorException(error);
         }
 
         return "Update payload is uploaded!";
--- src/libs/updater.ts
+++ src/libs/updater.ts
@@ -1,13 +1,16 @@
+import { copyFileSync, rmSync } from "fs";
 import { spawn } from "child_process";
-import { copyFileSync } from "fs";
+import { dirname, join } from "path";
 import {
     upload_update_from_dir,
     upload_update_to_dir,
+    checkOrCreateDir,
+    update_root_dir,
 } from "./upload-file-handler";
-import { join } from "path";
 import { glob } from "glob";
+import { InternalServerErrorException } from "@nestjs/common";
 
-export async function getUpdateFiles(): Promise<void> {
+export async function mergeUpdateFiles(): Promise<void> {
     const files = await glob(upload_update_from_dir + "/**/*");
 
     // Run the command only if update file is exist !
@@ -18,14 +21,39 @@ export async function getUpdateFiles(): Promise<void> {
                 upload_update_to_dir,
                 `${f.split("payload").pop()}`,
             );
-            await copyFileSync(f, dest);
+
+            // Create a folder if it doesn't exist
+            try {
+                await checkOrCreateDir(dirname(dest));
+            } catch (error) {
+                throw new InternalServerErrorException(error);
+            }
+
+            // SOON will be fixed for subfolder copy!
+            // Copy update file
+            try {
+                await copyFileSync(f, dest);
+            } catch (error) {
+                throw new InternalServerErrorException(error);
+            }
         }
 
-        // Restart backend server
-        spawn("pm2 restart permata-kasir-backend");
+        // Remove / delete everything inside update folder
+        try {
+            await rmSync(update_root_dir, { recursive: true });
+        } catch (error) {
+            throw new InternalServerErrorException(error);
+        }
     }
 }
 
-export function executeUpdate(): Promise<void> {
-    return getUpdateFiles();
+export async function executeUpdate(
+    reload_server: boolean = true,
+): Promise<void> {
+    await mergeUpdateFiles();
+
+    // Restart backend server (PRODUCTION ONLY)
+    if (reload_server) {
+        spawn("pm2 restart permata-kasir-backend");
+    }
 }
