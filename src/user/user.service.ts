b0142d9537548af08f8559fa391db218c880f03f
Izzat Alharis <izzatalharist@gmail.com>
2025-12-03 10:20:38 +0700

    Fix relation insert - user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,84 +5,87 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class UserService {
-  private readonly findOneKeys: Prisma.UserSelect = {
-    id: true,
-    nama: true,
-    tlp: true,
-    password: true,
-    foto: true,
-    online: true,
-    lastOnline: true,
-    active: true,
-    deactivatedAt: true,
-    createdAt: true,
-    updatedAt: true,
-  };
+    private readonly findOneKeys: Prisma.UserSelect = {
+        id: true,
+        nama: true,
+        tlp: true,
+        password: true,
+        foto: true,
+        online: true,
+        lastOnline: true,
+        active: true,
+        deactivatedAt: true,
+        createdAt: true,
+        updatedAt: true,
+    };
 
-  constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: any): Promise<User> {
-    let newData: Prisma.UserCreateInput = { ...data };
+    async create(data: any): Promise<User> {
+        let newData: any = { ...data };
 
-    // Konfigurasi timestamp
-    const thisTime = new Date().toISOString();
-    newData.createdAt = thisTime;
-    newData.updatedAt = thisTime;
+        // Konfigurasi timestamp
+        const thisTime = new Date().toISOString();
+        newData.createdAt = thisTime;
+        newData.updatedAt = thisTime;
 
-    // Enkripsi password
-    newData.password = encryptPassword(newData.password);
+        // Enkripsi password
+        newData.password = encryptPassword(newData.password);
 
-    // Fix active value (if client send through FormData)
-    if (newData.active) {
-      if (typeof newData.active == "string") {
-        newData.active = newData.active == "1" ? true : false;
-      }
+        // Parent table
+        newData.adminId = parseInt(data.adminId);
+
+        // Fix active value (if client send through FormData)
+        if (newData.active) {
+            if (typeof newData.active == "string") {
+                newData.active = newData.active == "1" ? true : false;
+            }
+        }
+
+        // Save a new data
+        return this.prisma.user.create({ data: newData });
     }
 
-    // Save a new data
-    return this.prisma.user.create({ data: newData });
-  }
+    async update(where: Prisma.UserWhereUniqueInput, data: any): Promise<User> {
+        let updatedData: Prisma.UserUpdateInput = { ...data };
 
-  async update(where: Prisma.UserWhereUniqueInput, data: any): Promise<User> {
-    let updatedData: Prisma.UserUpdateInput = { ...data };
+        // Konfigurasi timestamp
+        const thisTime = new Date().toISOString();
+        updatedData.updatedAt = thisTime;
 
-    // Konfigurasi timestamp
-    const thisTime = new Date().toISOString();
-    updatedData.updatedAt = thisTime;
+        // Save updated data
+        return this.prisma.user.update({ where, data: updatedData });
+    }
 
-    // Save updated data
-    return this.prisma.user.update({ where, data: updatedData });
-  }
+    async findAll(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.UserWhereUniqueInput;
+        where?: Prisma.UserWhereInput;
+        orderBy?: Prisma.UserOrderByWithRelationInput;
+    }): Promise<User[]> {
+        const { skip, take, cursor, where, orderBy } = params;
+        return this.prisma.user.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
 
-  async findAll(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.UserWhereUniqueInput;
-    where?: Prisma.UserWhereInput;
-    orderBy?: Prisma.UserOrderByWithRelationInput;
-  }): Promise<User[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.user.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
-  }
+    async findOne(params: {
+        select?: Prisma.UserSelect;
+        where: Prisma.UserWhereUniqueInput;
+    }): Promise<User | null> {
+        const { select, where } = params;
+        return this.prisma.user.findUnique({
+            select: { ...this.findOneKeys, ...select },
+            where,
+        });
+    }
 
-  async findOne(params: {
-    select?: Prisma.UserSelect;
-    where: Prisma.UserWhereUniqueInput;
-  }): Promise<User | null> {
-    const { select, where } = params;
-    return this.prisma.user.findUnique({
-      select: { ...this.findOneKeys, ...select },
-      where,
-    });
-  }
-
-  async remove(where: Prisma.UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.delete({ where });
-  }
+    async remove(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({ where });
+    }
 }
