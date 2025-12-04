631784917626754473758a8a4cef0558f96b7e77
Izzat Alharis <izzatalharist@gmail.com>
2025-12-04 11:26:17 +0700

    Fix app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { join } from "path";
 @Module({
     imports: [
         ServeStaticModule.forRoot({
-            rootPath: join(__dirname, "dist", "client"),
+            rootPath: join(__dirname, "client"),
         }),
         ConfigModule.forRoot({ isGlobal: true }),
         AdminModule,
