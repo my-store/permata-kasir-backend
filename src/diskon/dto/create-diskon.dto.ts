3ee3ded59feeaebd05235c407f906521134c389b
Izzat Alharis <izzatalharist@gmail.com>
2025-12-20 21:40:13 +0700

    Add insert validation on diskon route

--- src/diskon/dto/create-diskon.dto.ts
+++ src/diskon/dto/create-diskon.dto.ts
@@ -1,1 +1,12 @@
-export class CreateDiskonDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateDiskonDto {
+    @IsNotEmpty()
+    keterangan: string;
+
+    @IsNotEmpty()
+    nilai: string;
+
+    @IsNotEmpty()
+    tokoId: string;
+}
