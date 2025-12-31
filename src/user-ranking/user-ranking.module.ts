b1ba1333c390e50c627f0160d4a87096f12f589c
Izzat Alharis <izzatalharist@gmail.com>
2025-12-31 21:07:37 +0700

    Fix user ranking bugs

--- src/user-ranking/user-ranking.module.ts
+++ src/user-ranking/user-ranking.module.ts
@@ -1,9 +1,10 @@
 import { UserRankingController } from "./user-ranking.controller";
 import { UserRankingService } from "./user-ranking.service";
+import { PrismaService } from "src/prisma.service";
 import { Module } from "@nestjs/common";
 
 @Module({
     controllers: [UserRankingController],
-    providers: [UserRankingService],
+    providers: [UserRankingService, PrismaService],
 })
 export class UserRankingModule {}
